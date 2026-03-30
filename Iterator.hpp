b51eb861f11f0bfc39d4f8c69e6a7a9621fd7244
Яна <yanaakipova@gmail.com>
2026-03-30 17:33:14 +0300

    iterator realization

---
+++ Iterator.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include <iterator>
+template<class T>
+class Iterator{
+private:
+    T* m_ptr;
+public:
+    // типы для итератора
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = T;
+    using pointer = T*;
+    using reference = T&;
+
+    Iterator(T* ptr): m_ptr(ptr){}// список инициализаций
+
+    reference operator*() const {
+        return *m_ptr;
+    }
+
+    pointer operator->(){
+        return m_ptr;
+    }
+
+    Iterator& operator++(){
+        ++m_ptr;
+        return *this;
+    }
+
+    Iterator operator++(int){
+        Iterator old = *this;
+        ++(*this);
+        return old;
+    }
+
+    bool operator==(const Iterator& other) const{
+        return m_ptr == other.m_ptr;
+    }
+};
+
+template<class T>
+class ConstIterator{
+private:
+    const T* m_ptr;
+public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = T;
+    using difference_type = ptrdiff_t;
+    using pointer = const T*;
+    using reference = const T&;
+
+    ConstIterator(const T* ptr) : m_ptr(ptr) {}
+    
+    reference operator*() const{
+        return *m_ptr;
+    }
+
+    pointer operator->() const{
+        return m_ptr;
+    }
+
+    ConstIterator& operator++() {
+        ++m_ptr;
+        return *this;
+    }
+    
+    ConstIterator operator++(int) {
+        ConstIterator tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+
+     bool operator==(const ConstIterator& other) const { 
+        return m_ptr == other.m_ptr; 
+    }
+};
