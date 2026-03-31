d2a6e612ee5d4754e2590116dd83fecc774e75d8
Яна <yanaakipova@gmail.com>
2026-03-31 08:54:35 +0300

    map-reduce realization

--- MapReduce.hpp
+++ MapReduce.hpp
@@ -3,42 +3,94 @@
 #include "ArraySequence.hpp"
 
 template<class T, class U>
-Sequence<U>* MapWithIndex(Sequence<T>* seq, U (*func)(T,size_t)){
-    if(seq == nullptr){
-        throw std::invalid_argument("MapWithIndex: аргумент не передан");
+Sequence<U>* MapWithIndex(Sequence<T>& seq, U (*func)(const T&,size_t)){
+    size_t len = seq.GetLength();
+    ArraySequence<U>* result = new ArraySequence<U>(len);
+    size_t i=0;
+    for (auto it = seq.begin(); it != seq.end(); ++it,i++){
+        result->Set(i,func(*it,i));
     }
-    size_t len = seq->GetLength();
-    U* result = new U[len];
-
-    for (size_t i = 0; i < len; i++){
-        result[i] = func(seq->Get(i), i);
-    }
-    auto* new_seq = new ArraySequence<U>(result, len);
-    delete[] result;
-    return new_seq;
+    return result;
 }
 
+
 template<class T>
-Sequence<T>* Skip(Sequence<T>* seq, size_t n){
-    if(seq == nullptr){
-        throw std::invalid_argument("Skip: аргумент не передан");
-    }
-    if(n >= seq->GetLength()){
+Sequence<T>* Skip(Sequence<T>& seq, size_t n){
+    size_t len = seq.GetLength();
+    if (n >= len) {
         return new ArraySequence<T>();
     }
-    return seq->GetSubsequence(n, seq->GetLength() - 1);
+    size_t new_len = len - n;
+    ArraySequence<T>* result = new ArraySequence<T>();
+
+    auto it = seq.begin();
+    for(size_t i = 0; i < n; ++i){
+        ++it;
+    }
+    for(; it <= seq.end(); ++it){
+        result->Append(*it);
+    }
+    return result;
 }
 
 template<class T>
-Sequence<T>* Take(Sequence<T>* seq, size_t n){
-    if(seq == nullptr){
-        throw std::invalid_argument("Take: аргумент не передан");
-    }
+Sequence<T>* Take(Sequence<T>& seq, size_t n){
+    size_t len = seq.GetLength();
     if (n == 0) {
         return new ArraySequence<T>();
     }
-    if (n >= seq->GetLength()){
-        n = seq->GetLength();
+    if (n >= len){
+        n = len;
     }
-    return seq->GetSubsequence(0,n-1);
+    ArraySequence<T>* result = new ArraySequence<T>();
+    for(auto it = seq.begin(); it != seq.end(); ++it){
+        result->Append(*it);
+    }
+    return result;
+}
+
+template<class T, class U>
+Sequence<std::pair<T,U>>* Zip(Sequence<T>& seq1, Sequence<U>& seq2){
+    size_t min_len = min(seq1.GetLength(), seq2.GetLength());
+    ArraySequence<std::pair<T,U>>* result = new ArraySequence<std::pair<T,U>>;
+
+    auto it1 = seq1.begin;
+    auto it2 = seq2.begin;
+
+    for (size_t i = 0; i < minLen; ++i, ++it1, ++it2) {
+        result->Append({*it1, *it2});
+    }
+
+    return result
+}
+
+template<class T, class U>
+Sequence<U>* FlatMap(Sequence<T>& seq, Sequence<U>* (*func)(const T&)) {
+    auto* result = new ArraySequence<U>();
+    
+    for (auto it = seq.begin(); it != seq.end(); ++it) {
+        auto* mapped = func(*it);
+        for (auto mit = mapped->begin(); mit != mapped->end(); ++mit) {
+            result->Append(*mit);
+        }
+        delete mapped;
+    }
+    
+    return result;
+}
+
+template<class T>
+Sequence<T>* Range(T start, T end, T step = 1){
+    if (step == 0){
+        trown std::invalid_argument("Range: шаг не может быть равным 0");
+    } 
+    ArraySequence<T>* result = new ArraySequence<T>();
+    T current = start;
+    while ((step > 0 && current < end) || (step < 0 && current > end)) {
+        result->Append(current);
+        current += step;
+    }
+    
+    return result;
+
 }
