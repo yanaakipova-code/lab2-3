8d93aa640d5851bc1e4101a9a1ccd62a482eca47
Яна <yanaakipova@gmail.com>
2026-04-05 18:20:16 +0300

    Bit sequence corrected

--- BitSequence.cpp
+++ BitSequence.cpp
@@ -1,148 +1,323 @@
 #include "BitSequence.hpp"
+#include "ArraySequence.hpp" 
 #include <memory>
-#include <cstddef> 
+#include <cstddef>
+#include <cstdint>
 #include <stdexcept>
 
-BitSequence::BitSequence(bool* bits, size_t count) {
-    Bit* bitItems = new Bit[count];
+BitSequence::BitSequence(bool* bits, size_t count) 
+    : m_bit_count(count)
+{
+    size_t cells = GetCellsCount();
+    m_data = new DynamicArray<uint32_t>(cells);
+    // каждая ячейка 0
+    for (size_t i = 0; i < cells; i++) {
+        m_data->Set(i, 0);
+    }
+    //каждую ячейку заполняем значением
     for (size_t i = 0; i < count; i++) {
-        bitItems[i] = Bit(bits[i]);
+        if (bits[i]) {
+            SetBit(i, true);
+        }
     }
-    m_bits = new ArraySequence<Bit>(bitItems, count);
-    delete[] bitItems;
 }
 
-BitSequence::BitSequence(size_t size) {
-    Bit* bitItems = new Bit[size];
-    for (size_t i = 0; i < size; i++) {
-        bitItems[i] = Bit(false);
+BitSequence::BitSequence(size_t size) 
+    : m_bit_count(size)
+{
+    size_t cells = GetCellsCount();
+    m_data = new DynamicArray<uint32_t>(cells);
+    
+    for (size_t i = 0; i < cells; i++) {
+        m_data->Set(i, 0);
     }
-    m_bits = new ArraySequence<Bit>(bitItems, size);
-    delete[] bitItems;
 }
 
-BitSequence::BitSequence(const BitSequence& other) {
-    m_bits = new ArraySequence<Bit>(*other.m_bits);
+BitSequence::BitSequence(const BitSequence& other) 
+    : m_bit_count(other.m_bit_count)
+{
+    size_t cells = GetCellsCount();
+    m_data = new DynamicArray<uint32_t>(cells);
+    
+    for (size_t i = 0; i < cells; i++) {
+        m_data->Set(i, other.m_data->Get(i));
+    }
 }
 
 BitSequence::~BitSequence() {
-    delete m_bits;
+    delete m_data;
 }
 
-Bit BitSequence::GetFirst() const { 
-    return m_bits->GetFirst(); 
+
+Bit BitSequence::GetFirst() const {
+    if (m_bit_count == 0) {
+        throw std::out_of_range("BitSequence пуст");
+    }
+    return Get(0);
 }
 
-Bit BitSequence::GetLast() const { 
-    return m_bits->GetLast();
+Bit BitSequence::GetLast() const {
+    if (m_bit_count == 0) {
+        throw std::out_of_range("BitSequence пуст");
+    }
+    return Get(m_bit_count - 1);
 }
 
 Bit BitSequence::Get(size_t index) const {
-    return m_bits->Get(index); 
+    return Bit(GetBit(index));
 }
 
-size_t BitSequence::GetLength() const { 
-    return m_bits->GetLength(); 
+size_t BitSequence::GetLength() const {
+    return m_bit_count;
 }
 
 Sequence<Bit>* BitSequence::GetSubsequence(size_t start_index, size_t end_index) const {
-    return m_bits->GetSubsequence(start_index, end_index);
+    if (start_index > end_index || end_index >= m_bit_count) {
+        throw std::out_of_range("BitSequence::GetSubsequence: неверные индексы");
+    }
+    
+    size_t new_size = end_index - start_index + 1;
+    bool* temp_bits = new bool[new_size];
+    
+    for (size_t i = 0; i < new_size; i++) {
+        temp_bits[i] = GetBit(start_index + i);
+    }
+    
+    BitSequence* result = new BitSequence(temp_bits, new_size);
+    delete[] temp_bits;
+    
+    return result;
 }
 
 void BitSequence::Append(Bit temp) {
-    m_bits->Append(temp); 
+    size_t new_size = m_bit_count + 1;
+    size_t old_cells = GetCellsCount();
+    m_bit_count = new_size;
+    size_t new_cells = GetCellsCount();
+    
+    if (new_cells > old_cells) {
+        m_data->Resize(new_cells);
+        m_data->Set(new_cells - 1, 0);
+    }
+    
+    SetBit(m_bit_count - 1, temp.GetValue());
 }
 
 void BitSequence::Prepend(Bit temp) {
-    m_bits->Prepend(temp); 
+    InsertAt(temp, 0);
 }
 
 void BitSequence::InsertAt(Bit temp, size_t index) {
-    m_bits->InsertAt(temp, index); 
+    if (index > m_bit_count) {
+        throw std::out_of_range("BitSequence::InsertAt: индекс вне диапазона");
+    }
+    
+    size_t new_size = m_bit_count + 1;
+    size_t old_cells = GetCellsCount();
+    m_bit_count = new_size;
+    size_t new_cells = GetCellsCount();
+    
+    if (new_cells > old_cells) {
+        m_data->Resize(new_cells);//расширяем массив
+        m_data->Set(new_cells - 1, 0);
+    }
+    
+    for (size_t i = m_bit_count - 1; i > index; i--) {
+        bool bit = GetBit(i - 1);
+        SetBit(i, bit);
+    }
+    
+    SetBit(index, temp.GetValue());
 }
 
 Sequence<Bit>* BitSequence::Concat(Sequence<Bit>* other) const {
-    return m_bits->Concat(other);
+    if (other == nullptr) {
+        throw std::invalid_argument("BitSequence::Concat: нулевой указатель");
+    }
+    
+    size_t total_size = m_bit_count + other->GetLength();
+    bool* temp_bits = new bool[total_size];
+    
+    for (size_t i = 0; i < m_bit_count; i++) {
+        temp_bits[i] = GetBit(i);
+    }
+    
+    for (size_t i = 0; i < other->GetLength(); i++) {
+        temp_bits[m_bit_count + i] = other->Get(i).GetValue();
+    }
+    
+    BitSequence* result = new BitSequence(temp_bits, total_size);
+    delete[] temp_bits;
+    
+    return result;
 }
 
 Sequence<Bit>* BitSequence::Map(Bit (*func)(Bit)) {
-    return m_bits->Map(func);
+    BitSequence* result = new BitSequence(m_bit_count);
+    
+    for (size_t i = 0; i < m_bit_count; i++) {
+        Bit transformed = func(Get(i));
+        result->SetBit(i, transformed.GetValue());
+    }
+    
+    return result;
 }
 
 Sequence<Bit>* BitSequence::Where(bool (*predicate)(Bit)) {
-    return m_bits->Where(predicate);
+    size_t count = 0;
+    for (size_t i = 0; i < m_bit_count; i++) {
+        if (predicate(Get(i))) {
+            count++;
+        }
+    }
+    
+    bool* temp_bits = new bool[count];
+    size_t index = 0;
+    
+    for (size_t i = 0; i < m_bit_count; i++) {
+        if (predicate(Get(i))) {
+            temp_bits[index++] = GetBit(i);
+        }
+    }
+    
+    BitSequence* result = new BitSequence(temp_bits, count);
+    delete[] temp_bits;
+    
+    return result;
 }
 
 Bit BitSequence::Reduce(Bit (*func)(Bit, Bit), Bit initial) {
-    return m_bits->Reduce(func, initial);
+    Bit result = initial;
+    
+    for (size_t i = 0; i < m_bit_count; i++) {
+        result = func(result, Get(i));
+    }
+    
+    return result;
 }
 
 Option<Bit> BitSequence::TryGetFirst(bool (*predicate)(Bit)) const {
-    return m_bits->TryGetFirst(predicate);
+    for (size_t i = 0; i < m_bit_count; i++) {
+        Bit elem = Get(i);
+        if (predicate == nullptr || predicate(elem)) {
+            return Option<Bit>::Some(elem);
+        }
+    }
+    return Option<Bit>::None();
 }
 
 Option<Bit> BitSequence::TryGetLast(bool (*predicate)(Bit)) const {
-    return m_bits->TryGetLast(predicate);
+    for (size_t i = m_bit_count; i > 0; i--) {
+        Bit elem = Get(i - 1);
+        if (predicate == nullptr || predicate(elem)) {
+            return Option<Bit>::Some(elem);
+        }
+    }
+    return Option<Bit>::None();
+}
+
+void BitSequence::SetBit(size_t index, bool value) {
+    if (index >= m_bit_count) {
+        throw std::out_of_range("BitSequence::SetBit: индекс вне диапазона");
+    }
+    
+    size_t cell = GetCellIndex(index);
+    size_t offset = GetBitOffset(index);
+    uint32_t word = m_data->Get(cell);
+    
+    if (value) {
+        word |= (1u << offset);
+    } else {
+        word &= ~(1u << offset);
+    }
+    
+    m_data->Set(cell, word);
+}
+
+bool BitSequence::GetBit(size_t index) const {
+    if (index >= m_bit_count) {
+        throw std::out_of_range("BitSequence::GetBit: индекс вне диапазона");
+    }
+    
+    size_t cell = GetCellIndex(index);
+    size_t offset = GetBitOffset(index);
+    uint32_t word = m_data->Get(cell);
+    
+    return (word >> offset) & 1;
 }
 
 std::unique_ptr<BitSequence> BitSequence::And(const BitSequence& other) const {
-    if (GetLength() != other.GetLength()) {
+    if (m_bit_count != other.m_bit_count) {
         throw std::invalid_argument("BitSequence::And: длины не совпадают");
     }
     
-    auto result = std::make_unique<BitSequence>(GetLength());
-    for (size_t i = 0; i < GetLength(); i++) {
-        bool bitValue = Get(i).GetValue() & other.Get(i).GetValue();
-        result->SetBit(i, bitValue);
+    auto result = std::make_unique<BitSequence>(m_bit_count);
+    size_t cells = GetCellsCount();
+    
+    for (size_t i = 0; i < cells; i++) {
+        uint32_t val = m_data->Get(i) & other.m_data->Get(i);
+        result->m_data->Set(i, val);
     }
+    
+    result->ClearUnusedBits();
     return result;
 }
 
 std::unique_ptr<BitSequence> BitSequence::Or(const BitSequence& other) const {
-    if (GetLength() != other.GetLength()) {
+    if (m_bit_count != other.m_bit_count) {
         throw std::invalid_argument("BitSequence::Or: длины не совпадают");
     }
     
-    auto result = std::make_unique<BitSequence>(GetLength());
-    for (size_t i = 0; i < GetLength(); i++) {
-        bool bitValue = Get(i).GetValue() | other.Get(i).GetValue();
-        result->SetBit(i, bitValue);
+    auto result = std::make_unique<BitSequence>(m_bit_count);
+    size_t cells = GetCellsCount();
+    
+    for (size_t i = 0; i < cells; i++) {
+        uint32_t val = m_data->Get(i) | other.m_data->Get(i);
+        result->m_data->Set(i, val);
     }
+    
+    result->ClearUnusedBits();
     return result;
 }
 
 std::unique_ptr<BitSequence> BitSequence::Xor(const BitSequence& other) const {
-    if (GetLength() != other.GetLength()) {
+    if (m_bit_count != other.m_bit_count) {
         throw std::invalid_argument("BitSequence::Xor: длины не совпадают");
     }
     
-    auto result = std::make_unique<BitSequence>(GetLength());
-    for (size_t i = 0; i < GetLength(); i++) {
-        bool bitValue = Get(i).GetValue() ^ other.Get(i).GetValue();
-        result->SetBit(i, bitValue);
+    auto result = std::make_unique<BitSequence>(m_bit_count);
+    size_t cells = GetCellsCount();
+    
+    for (size_t i = 0; i < cells; i++) {
+        uint32_t val = m_data->Get(i) ^ other.m_data->Get(i);
+        result->m_data->Set(i, val);
     }
+    
+    result->ClearUnusedBits();
     return result;
 }
 
 std::unique_ptr<BitSequence> BitSequence::Not() const {
-    auto result = std::make_unique<BitSequence>(GetLength());
-    for (size_t i = 0; i < GetLength(); i++) {
-        bool bitValue = !Get(i).GetValue();
-        result->SetBit(i, bitValue);
+    auto result = std::make_unique<BitSequence>(m_bit_count);
+    size_t cells = GetCellsCount();
+    
+    for (size_t i = 0; i < cells; i++) {
+        uint32_t val = ~(m_data->Get(i));
+        result->m_data->Set(i, val);
     }
+    
+    result->ClearUnusedBits();
     return result;
 }
 
-void BitSequence::SetBit(size_t index, bool value) {
-    m_bits->Set(index, Bit(value));
-}
-
-bool BitSequence::GetBit(size_t index) const {
-    return Get(index).GetValue();
-}
-
 std::unique_ptr<ArraySequence<Bit>> BitSequence::ToMutable() const {
-    return std::make_unique<ArraySequence<Bit>>(*m_bits);
+    auto result = std::make_unique<ArraySequence<Bit>>();
+    
+    for (size_t i = 0; i < m_bit_count; i++) {
+        result->Append(Bit(GetBit(i)));
+    }
+    
+    return result;
 }
 
 BitSequence BitSequence::operator&(const BitSequence& other) const {
@@ -165,34 +340,3 @@ BitSequence BitSequence::operator~() const {
     return *result;
 }
 
-Bit& BitSequence::operator[](size_t index) {
-    return const_cast<Bit&>(m_bits->operator[](index));
-}
-
-const Bit& BitSequence::operator[](size_t index) const {
-    return m_bits->operator[](index);
-}
-
-Iterator<Bit> BitSequence::begin() {
-    return m_bits->begin();
-}
-
-Iterator<Bit> BitSequence::end() {
-    return m_bits->end();
-}
-
-ConstIterator<Bit> BitSequence::begin() const {
-    return ConstIterator<Bit>(&(*m_bits->begin()));
-}
-
-ConstIterator<Bit> BitSequence::end() const {
-    return ConstIterator<Bit>(&(*m_bits->end()));
-}
-
-ConstIterator<Bit> BitSequence::cbegin() const {
-    return ConstIterator<Bit>(&(*m_bits->cbegin()));
-}
-
-ConstIterator<Bit> BitSequence::cend() const {
-    return ConstIterator<Bit>(&(*m_bits->cend()));
-}
--- BitSequence.hpp
+++ BitSequence.hpp
@@ -1,15 +1,44 @@
 #pragma once
 #include <memory>
-#include <cstddef> 
+#include <cstddef>
+#include <cstdint>
+#include <stdexcept>
 #include "Sequence.hpp"
-#include "ArraySequence.hpp"
+#include "DynamicArray.hpp"
 #include "Bit.hpp"
 #include "Option.hpp"
 #include "Iterator.hpp"
+#define BITS_PER_CELL 32
 
-class BitSequence: public Sequence<Bit>{
+class BitSequence : public Sequence<Bit> {
 private:
-    ArraySequence<Bit>* m_bits;
+    DynamicArray<uint32_t>* m_data;
+    size_t m_bit_count;
+    
+    size_t GetCellIndex(size_t bit_index) const {
+        return bit_index / BITS_PER_CELL;
+    }
+    
+    size_t GetBitOffset(size_t bit_index) const {
+        return bit_index % BITS_PER_CELL;
+    }
+    
+    size_t GetCellsCount() const {
+        return (m_bit_count + BITS_PER_CELL - 1) / BITS_PER_CELL;
+    }
+    
+    void ClearUnusedBits() {
+        if (m_bit_count == 0) return;
+        
+        size_t cells = GetCellsCount();
+        size_t lastBits = GetBitOffset(m_bit_count);
+        
+        if (lastBits != 0) {
+            uint32_t mask = (1u << lastBits) - 1;
+            uint32_t lastVal = m_data->Get(cells - 1);
+            m_data->Set(cells - 1, lastVal & mask);
+        }
+    }
 
 public:    
     BitSequence(bool* bits, size_t count);
@@ -22,31 +51,34 @@ public:
     Bit Get(size_t index) const override;
     size_t GetLength() const override;
     Sequence<Bit>* GetSubsequence(size_t start_index, size_t end_index) const override;
+
     void Append(Bit temp) override;
     void Prepend(Bit temp) override;
     void InsertAt(Bit temp, size_t index) override;
     Sequence<Bit>* Concat(Sequence<Bit>* other) const override;
-    
+
     Sequence<Bit>* Map(Bit (*func)(Bit)) override;
     Sequence<Bit>* Where(bool (*predicate)(Bit)) override;
     Bit Reduce(Bit (*func)(Bit, Bit), Bit initial) override;
+
     Option<Bit> TryGetFirst(bool (*predicate)(Bit) = nullptr) const override;
     Option<Bit> TryGetLast(bool (*predicate)(Bit) = nullptr) const override;
-
-    std::unique_ptr<BitSequence> And(const BitSequence& other) const;
-    std::unique_ptr<BitSequence> Or(const BitSequence& other) const;
-    std::unique_ptr<BitSequence> Xor(const BitSequence& other) const;  
-    std::unique_ptr<BitSequence> Not() const;                          
 
     void SetBit(size_t index, bool value);
     bool GetBit(size_t index) const;
 
+    std::unique_ptr<BitSequence> And(const BitSequence& other) const;
+    std::unique_ptr<BitSequence> Or(const BitSequence& other) const;
+    std::unique_ptr<BitSequence> Xor(const BitSequence& other) const;
+    std::unique_ptr<BitSequence> Not() const;
+    
     std::unique_ptr<ArraySequence<Bit>> ToMutable() const;
     
     BitSequence operator&(const BitSequence& other) const;
     BitSequence operator|(const BitSequence& other) const;
     BitSequence operator^(const BitSequence& other) const;
     BitSequence operator~() const;
+    
     Bit& operator[](size_t index);
     const Bit& operator[](size_t index) const;
 
@@ -54,7 +86,6 @@ public:
     Iterator<Bit> end() override;
     ConstIterator<Bit> begin() const override;
     ConstIterator<Bit> end() const override;
-
     ConstIterator<Bit> cbegin() const override;
     ConstIterator<Bit> cend() const override;
 };
