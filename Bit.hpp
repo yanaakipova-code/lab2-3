4f87cf564c3e6bee86d06ae8d0d85d1e04ea3366
Яна <yanaakipova@gmail.com>
2026-04-12 20:40:15 +0300

    Bit realization

--- Bit.hpp
+++ Bit.hpp
@@ -1,17 +1,36 @@
 #pragma once
-#include <bitset>
+#include <concepts>
+
+template<std::integral T>
 class Bit {
 private:
-    unsigned int m_mask;
+    T m_mask;
+    
 public:
     Bit() : m_mask(0) {}
-    Bit(unsigned int other): m_mask(other){}
-
-    int GetBit(size_t i) const {
-        return (m_mask >> i) & 1;
+    Bit(T value) : m_mask(value) {}
+    
+    bool GetBit(size_t i) const {
+        return ((m_mask >> i) & static_cast<T>(1)) != static_cast<T>(0);
     }
-
-    unsigned int GetMask() const {
+    
+    T GetMask() const {
         return m_mask;
+    }
+    
+    Bit<T> operator&(const Bit<T>& other) const {
+        return Bit<T>(m_mask & other.m_mask);
+    }
+    
+    Bit<T> operator|(const Bit<T>& other) const {
+        return Bit<T>(m_mask | other.m_mask);
+    }
+    
+    Bit<T> operator^(const Bit<T>& other) const {
+        return Bit<T>(m_mask ^ other.m_mask);
+    }
+    
+    Bit<T> operator~() const {
+        return Bit<T>(~m_mask);
     }
 };
