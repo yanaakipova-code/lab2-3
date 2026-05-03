9e4efceefd2d6772a57fb5cdb6dae4dd0a93593b
Яна <yanaakipova@gmail.com>
2026-05-03 09:08:03 +0300

    main file

--- lab_3/main.cpp
+++ lab_3/main.cpp
@@ -5,6 +5,7 @@
 #include "menu/QueueMenu.hpp"
 #include "menu/SetMenu.hpp"
 #include "menu/MatrixMenu.hpp"
+#include "menu/ShootingMenu.hpp"
 #include "Complex.hpp"
 #include "Person.hpp"
 
@@ -20,13 +21,15 @@ int main() {
         "Очередь",
         "Множество",
         "Матрица",
+        "Расчет траектории (пристрелка)",
         "Выход"
     };
+    int mainItemCount = 5;
     
     while (true) {
-        int choice = RunMenu("ЛАБОРАТОРНАЯ РАБОТА №3", mainItems, 4);
+        int choice = RunMenu("ЛАБОРАТОРНАЯ РАБОТА №3", mainItems, mainItemCount);
         
-        if (choice == -1 || choice == 3) break;
+        if (choice == -1 || choice == 4) break;
         
         if (choice == 0) {
             int type = SelectType("очереди");
@@ -43,14 +46,10 @@ int main() {
             else if (type == TYPE_PERSON) SetMenuWithType<Person>("Персона");
         }
         else if (choice == 2) {
-            int type = SelectType("матрицы");
-            if (type == TYPE_INT) MatrixMenuWithType<int>("int");
-            else if (type == TYPE_DOUBLE) MatrixMenuWithType<double>("double");
-            else if (type == TYPE_COMPLEX) {
-                ShowMessage("Матрица для комплексных чисел пока не реализована", true);
-            } else if (type == TYPE_PERSON) {
-                ShowMessage("Матрица для персон пока не реализована", true);
-            }
+            MatrixMenu();
+        }
+        else if (choice == 3) {
+            RunShootingMenu();
         }
     }
     
